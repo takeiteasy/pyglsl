019f8fb3be71986989d70d53e1f91c018eaa48df
George Watson <gigolo@hotmail.co.uk>
2025-02-03 11:29:44 +0000

    license as header

--- pyglsl/interface.py
+++ pyglsl/interface.py
@@ -1,3 +1,21 @@
+# Purely pythonic fast first-rate fully-functioning flexible feature-rich framework for fun
+#
+# Copyright (C) 2016 Nicholas Bishop
+# Copyright (C) 2025 George Watson
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import ast
 import attr
 import attrs
